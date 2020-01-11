a1af62ea4c6f8bb89aeec56d106c054f6a413eae
Wuata <lmgom875@gmail.com>
2020-01-11 13:17:02 -0500

    change function getInfo to displayWeatherInfo (full function for get the info and display on screen)

--- assets/javaWD.js
+++ assets/javaWD.js
@@ -5,45 +5,62 @@ $(document).ready(function(){
 
 var textBox = "";
 var inputLoc = "Hartford";
-var apiKey = "";
-var queryURL_Current = "http://api.openweathermap.org/data/2.5/weather?q="+inputLoc+"&appid="+apiKey;
-var queryURL_5Days = "http://api.openweathermap.org/data/2.5/forecast?q="+inputLoc+"&appid="+apiKey;
+var apiKey = "244e1909ec9b0b408cc09c27f2d725dd";
+var queryURL_Current = "http://api.openweathermap.org/data/2.5/weather?q="+inputLoc+"&units=imperial&appid="+apiKey;
+var queryURL_5Days = "http://api.openweathermap.org/data/2.5/forecast?q="+inputLoc+"&units=imperial&appid="+apiKey;
 
-getWeatherInfo();
+displayWeatherInfo();
+
+
 
 
 /* -------------------------- */
 /* Function*/
 /* -------------------------- */
-function getWeatherInfo(){
-     $.ajax({
-        url: queryURL_5Days,
-        method: "GET"
-        }).then(function(response_5Days){
-        var results_5Days = response_5Days;
-        //console.log(results_5Days);
-    })
+
+//Function fron get the api info
+function displayWeatherInfo(){
     $.ajax({
         url: queryURL_Current,
-        method: "GET"
-        }).then(function(response_Current){
-        var results_Current = response_Current;
-        //console.log(results_Current);
+        method: "GET",
+        }).then(function(responseCurrent){
+        var cityName = responseCurrent.name;
+        var icon = responseCurrent.weather[0].icon;
+        var iconUrl = "http://openweathermap.org/img/w/"+icon+".png";
+        var temp = responseCurrent.main.temp;
+        var Humidity = responseCurrent.main.humidity;
+        var windSpeed = responseCurrent.wind.speed;
+        $("#currentConditions1").append("<h4 id='h4CityName'>"+cityName);
+        $("#h4CityName").append("<img id='wicon' src='' alt='Weather Icon'>");
+        $("#wicon").attr('src',iconUrl);
+        $("#currentConditions1").append("<p>Temperature: "+temp+"&#8457;</p>");
+        $("#currentConditions1").append("<p>Humidity: "+Humidity+"%</p>");
+        $("#currentConditions1").append("<p>Wind Speed: "+windSpeed+" MPH</p>");
+
+        console.log(responseCurrent);
+        console.log(icon);
+        console.log(iconUrl);
         })
 }
 
+//Function to put the info on display
+function displayInfo (){
+    $("#currentConditions1").append("<p>"+cityName+"</p>");
+
+}
 
 //function to create locations button
-    function createBtnLoc(){
-        textBox = $("#seachBoxText").val();
-        if(textBox != ""){
-            $("#locBtn").prepend("<button class='btn btn-default btnLocation' id='btn btnLocation btn-"+textBox+"' type='button'>"+textBox+"</button></br>")
-        }
+function createBtnLoc(){
+    textBox = $("#seachBoxText").val();
+    if(textBox != ""){
+        $("#locBtn").prepend("<button class='btn btn-default btnLocation' id='btn btnLocation btn-"+textBox+"' type='button'>"+textBox+"</button></br>")
     }
+}
 
 /* -------------------------- */
 /* Events*/
 /* -------------------------- */
+
 
     $("#btnGo").click(function(){
         createBtnLoc();
